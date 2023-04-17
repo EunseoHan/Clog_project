1693275539b06e35d10a1415c3e9fcf42bff9a77
sw622 <sw622@naver.com>
2023-04-18 00:48:18 +0900

    1. AddCloset 수정

--- app/src/main/java/com/example/myapplication/AddCloset.kt
+++ app/src/main/java/com/example/myapplication/AddCloset.kt
@@ -12,12 +12,17 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Radio
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
@@ -37,20 +42,67 @@ class AddCloset : BaseActivity() {
     lateinit var onepiece: Button
     lateinit var shoes: Button
 
-    lateinit var outer_txtview: TextView
-    lateinit var outer_layout: LinearLayout
-    lateinit var top_txtview: TextView
-    lateinit var top_layout: LinearLayout
-    lateinit var bottom_txtview1: TextView
-    lateinit var bottom_layout1: LinearLayout
-    lateinit var bottom_txtview2: TextView
-    lateinit var bottom_layout2: LinearLayout
-    lateinit var onepiece_txtview1: TextView
-    lateinit var onepiece_layout1: LinearLayout
-    lateinit var onepiece_txtview2: TextView
-    lateinit var onepiece_layout2: LinearLayout
-    lateinit var shoes_txtview: TextView
-    lateinit var shoes_layout: LinearLayout
+    lateinit var type_detail_outer: LinearLayout
+    lateinit var type_detail_top: LinearLayout
+    lateinit var type_detail_bottom: LinearLayout
+    lateinit var type_detail_onepiece: LinearLayout
+    lateinit var type_detail_shoes: LinearLayout
+
+    lateinit var lining: LinearLayout
+    lateinit var thickness: LinearLayout
+    lateinit var length: LinearLayout
+
+    lateinit var cardigan: RadioButton
+    lateinit var jacket: RadioButton
+    lateinit var field_jacket: RadioButton
+    lateinit var leather_jacket: RadioButton
+    lateinit var fleece: RadioButton
+    lateinit var hoodie: RadioButton
+    lateinit var coat: RadioButton
+    lateinit var padded_coat: RadioButton
+
+    lateinit var non_sleeve: RadioButton
+    lateinit var short_sleeve: RadioButton
+    lateinit var shirt: RadioButton
+    lateinit var mtm: RadioButton
+    lateinit var hoodie_top: RadioButton
+    lateinit var sweater: RadioButton
+
+    lateinit var denim: RadioButton
+    lateinit var leggings: RadioButton
+    lateinit var skirt: RadioButton
+    lateinit var slacks: RadioButton
+    lateinit var jogger: RadioButton
+    lateinit var cotton: RadioButton
+
+    lateinit var non_sleeve_onpiece: RadioButton
+    lateinit var short_sleeve_onepiece: RadioButton
+    lateinit var long_sleeve_onepiece: RadioButton
+
+    lateinit var sneakers: RadioButton
+    lateinit var sandals: RadioButton
+    lateinit var slippers: RadioButton
+    lateinit var boots: RadioButton
+    lateinit var loafer: RadioButton
+    lateinit var rubber_boots: RadioButton
+
+    lateinit var outer_gr1: RadioGroup
+    lateinit var outer_gr2: RadioGroup
+    lateinit var top_gr1: RadioGroup
+    lateinit var top_gr2: RadioGroup
+    lateinit var bottom_gr1: RadioGroup
+    lateinit var bottom_gr2: RadioGroup
+    lateinit var onepiece_gr: RadioGroup
+    lateinit var shoes_gr1: RadioGroup
+    lateinit var shoes_gr2: RadioGroup
+
+    lateinit var lining_gr: RadioGroup
+    lateinit var thickness_gr: RadioGroup
+    lateinit var length_gr: RadioGroup
+
+    lateinit var length_3: RadioButton
+    lateinit var length_5: RadioButton
+    lateinit var length_9: RadioButton
 
     val PERM_STORAGE = 9
     val PERM_CAMERA = 10
@@ -73,27 +125,111 @@ class AddCloset : BaseActivity() {
         onepiece = findViewById<Button>(R.id.onepiece)
         shoes = findViewById<Button>(R.id.shoes)
 
-        outer_txtview = findViewById(R.id.type_detail_outer_txtview)
-        top_txtview = findViewById(R.id.type_detail_top_txtview)
-        bottom_txtview1 = findViewById(R.id.type_detail_bottom_txtview1)
-        bottom_txtview2 = findViewById(R.id.type_detail_bottom_txtview2)
-        onepiece_txtview1 = findViewById(R.id.type_detail_onepiece_txtview1)
-        onepiece_txtview2 = findViewById(R.id.type_detail_onepiece_txtview2)
-        shoes_txtview = findViewById(R.id.type_detail_shoes_txtview)
+        type_detail_outer = findViewById(R.id.type_detail_outer)
+        type_detail_top = findViewById(R.id.type_detail_top)
+        type_detail_bottom = findViewById(R.id.type_detail_bottom)
+        type_detail_onepiece = findViewById(R.id.type_detail_onepiece)
+        type_detail_shoes = findViewById(R.id.type_detail_shoes)
 
-        outer_layout = findViewById(R.id.type_detail_outer_layout)
-        top_layout = findViewById(R.id.type_detail_top_layout)
-        bottom_layout1 = findViewById(R.id.type_detail_bottom_layout1)
-        bottom_layout2 = findViewById(R.id.type_detail_bottom_layout2)
-        onepiece_layout1 = findViewById(R.id.type_detail_onepiece_layout1)
-        onepiece_layout2 = findViewById(R.id.type_detail_onepiece_layout2)
-        shoes_layout = findViewById(R.id.type_detail_shoes_layout)
+        lining = findViewById(R.id.lining)
+        thickness = findViewById(R.id.thickness)
+        length = findViewById(R.id.length)
 
-        clickBtn(outer, outer_txtview, outer_layout)
-        clickBtn(top, top_txtview, top_layout)
-        clickBtn(bottom, bottom_txtview1, bottom_txtview2, bottom_layout1, bottom_layout2)
-        clickBtn(onepiece, onepiece_txtview1, onepiece_txtview2, onepiece_layout1, onepiece_layout2)
-        clickBtn(shoes, shoes_txtview, shoes_layout)
+        cardigan = findViewById(R.id.cardigan)
+        jacket = findViewById(R.id.jacket)
+        field_jacket = findViewById(R.id.field_jacket)
+        leather_jacket = findViewById(R.id.leather_jacket)
+        fleece = findViewById(R.id.fleece)
+        hoodie = findViewById(R.id.hoodie)
+        coat = findViewById(R.id.coat)
+        padded_coat = findViewById(R.id.padded_coat)
+
+        non_sleeve = findViewById(R.id.non_sleeve)
+        short_sleeve = findViewById(R.id.short_sleeve)
+        shirt = findViewById(R.id.shirt)
+        mtm = findViewById(R.id.mtm)
+        hoodie_top = findViewById(R.id.hoodie_top)
+        sweater = findViewById(R.id.sweater)
+
+        denim = findViewById(R.id.denim)
+        leggings = findViewById(R.id.leggings)
+        skirt = findViewById(R.id.skirt)
+        slacks = findViewById(R.id.slacks)
+        jogger = findViewById(R.id.jogger)
+        cotton = findViewById(R.id.cotton)
+
+        non_sleeve_onpiece = findViewById(R.id.non_sleeve_onepiece)
+        short_sleeve_onepiece = findViewById(R.id.short_sleeve_onepiece)
+        long_sleeve_onepiece = findViewById(R.id.long_sleeve_onepiece)
+
+        sneakers = findViewById(R.id.sneakers)
+        sandals = findViewById(R.id.sandals)
+        slippers = findViewById(R.id.slippers)
+        boots = findViewById(R.id.boots)
+        loafer = findViewById(R.id.loafer)
+        rubber_boots = findViewById(R.id.rubber_boots)
+
+        outer_gr1 = findViewById(R.id.outer_gr1)
+        outer_gr2 = findViewById(R.id.outer_gr2)
+        top_gr1 = findViewById(R.id.top_gr1)
+        top_gr2 = findViewById(R.id.top_gr2)
+        bottom_gr1 = findViewById(R.id.bottom_gr1)
+        bottom_gr2 = findViewById(R.id.bottom_gr2)
+        onepiece_gr = findViewById(R.id.onepiece_gr)
+        shoes_gr1 = findViewById(R.id.shoes_gr1)
+        shoes_gr2 = findViewById(R.id.shoes_gr2)
+
+        lining_gr = findViewById(R.id.lining_gr)
+        thickness_gr = findViewById(R.id.thickness_gr)
+        length_gr = findViewById(R.id.length_gr)
+
+        length_3 = findViewById(R.id.length_3)
+        length_5 = findViewById(R.id.length_5)
+        length_9 = findViewById(R.id.length_9)
+
+
+
+        clickBtn(outer, type_detail_outer)
+        clickBtn(top, type_detail_top)
+        clickBtn(bottom, type_detail_bottom)
+        clickBtn(onepiece, type_detail_onepiece)
+        clickBtn(shoes, type_detail_shoes)
+
+        clickRadio(cardigan, thickness)
+        clickRadio(jacket, thickness)
+        clickRadio(field_jacket)
+        clickRadio(leather_jacket)
+        clickRadio(fleece)
+        clickRadio(hoodie, lining)
+        clickRadio(coat, thickness)
+        clickRadio(padded_coat)
+
+        clickRadio(non_sleeve)
+        clickRadio(short_sleeve)
+        clickRadio(shirt)
+        clickRadio(mtm, lining)
+        clickRadio(hoodie_top, lining)
+        clickRadio(sweater, thickness)
+
+        clickRadio(denim, length)
+        clickRadio(leggings, length)
+        clickRadio(skirt, length)
+        clickRadio(slacks, length)
+        clickRadio(jogger, length)
+        clickRadio(cotton, length)
+
+        clickRadio(non_sleeve_onpiece, length)
+        clickRadio(short_sleeve_onepiece, length)
+        clickRadio(long_sleeve_onepiece, length)
+
+        clickRadio(sneakers)
+        clickRadio(sandals)
+        clickRadio(slippers)
+        clickRadio(boots)
+        clickRadio(loafer)
+        clickRadio(rubber_boots)
+
+        bottom_length9()
 
 
         back.setOnClickListener {
@@ -101,57 +237,73 @@ class AddCloset : BaseActivity() {
         }
 
 
+
         // 1. 공용저장소 권한이 있는지 확인
         requirePermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), PERM_STORAGE)
     }
 
-    fun clickBtn(btn:Button, txtview:TextView, layout: LinearLayout) {
+    fun clickBtn(btn:Button, layout: LinearLayout) {
         btn.setOnClickListener {
-            if (txtview.visibility == VISIBLE) {
+            if (layout.visibility == VISIBLE) {
                 backgroundClear()
                 visibleGone()
             } else {
                 backgroundClear()
                 visibleGone()
-                txtview.visibility = View.VISIBLE
+                radio_clear()
                 layout.visibility = View.VISIBLE
                 btn.setBackgroundColor(Color.parseColor("#BFCBE5"))
                 btn.setBackgroundResource(R.drawable.button_roud2)
             }
         }
     }
-    fun clickBtn(btn:Button, txtview:TextView, txtview2: TextView, layout: LinearLayout, layout2: LinearLayout) {
-        btn.setOnClickListener {
-            if (txtview.visibility == VISIBLE) {
-                backgroundClear()
-                visibleGone()
-            } else {
-                backgroundClear()
-                visibleGone()
-                txtview.visibility = View.VISIBLE
-                layout.visibility = View.VISIBLE
-                txtview2.visibility = View.VISIBLE
-                layout2.visibility = View.VISIBLE
-                btn.setBackgroundColor(Color.parseColor("#BFCBE5"))
-                btn.setBackgroundResource(R.drawable.button_roud2)
-            }
-        }
-    }
+
     fun visibleGone() {
-        outer_txtview.visibility = View.GONE
-        outer_layout.visibility = View.GONE
-        top_txtview.visibility = View.GONE
-        top_layout.visibility = View.GONE
-        bottom_txtview1.visibility = View.GONE
-        bottom_txtview2.visibility = View.GONE
-        bottom_layout1.visibility = View.GONE
-        bottom_layout2.visibility = View.GONE
-        onepiece_txtview1.visibility = View.GONE
-        onepiece_txtview2.visibility = View.GONE
-        onepiece_layout1.visibility = View.GONE
-        onepiece_layout2.visibility = View.GONE
-        shoes_txtview.visibility = View.GONE
-        shoes_layout.visibility = View.GONE
+        type_detail_outer.visibility = View.GONE
+        type_detail_top.visibility = View.GONE
+        type_detail_bottom.visibility = View.GONE
+        type_detail_onepiece.visibility = View.GONE
+        type_detail_shoes.visibility = View.GONE
+
+        visibleGone_radio()
+    }
+
+    fun clickRadio(rd:RadioButton, layout:LinearLayout) {
+        rd.setOnClickListener {
+            radio_clear()
+            visibleGone_radio()
+            layout.visibility = View.VISIBLE
+            rd.isChecked = true
+        }
+    }
+
+    fun clickRadio(rd:RadioButton) {
+        rd.setOnClickListener {
+            radio_clear()
+            visibleGone_radio()
+            rd.isChecked = true
+        }
+    }
+    fun radio_clear() {
+        outer_gr1.clearCheck()
+        outer_gr2.clearCheck()
+        top_gr1.clearCheck()
+        top_gr2.clearCheck()
+        bottom_gr1.clearCheck()
+        bottom_gr2.clearCheck()
+        onepiece_gr.clearCheck()
+        shoes_gr1.clearCheck()
+        shoes_gr2.clearCheck()
+
+        lining_gr.clearCheck()
+        thickness_gr.clearCheck()
+        length_gr.clearCheck()
+    }
+
+    fun visibleGone_radio() {
+        lining.visibility = View.GONE
+        thickness.visibility = View.GONE
+        length.visibility = View.GONE
     }
 
     fun backgroundClear() {
@@ -160,6 +312,23 @@ class AddCloset : BaseActivity() {
         bottom.setBackgroundResource(R.drawable.button_roud)
         onepiece.setBackgroundResource(R.drawable.button_roud)
         shoes.setBackgroundResource(R.drawable.button_roud)
+    }
+
+    fun bottom_length9() {
+        length_9.setOnClickListener {
+            if (denim.isChecked || leggings.isChecked || jogger.isChecked)
+                lining.visibility = VISIBLE
+            if (slacks.isChecked)
+                thickness.visibility = VISIBLE
+        }
+        length_3.setOnClickListener {
+            lining.visibility = GONE
+            thickness.visibility = GONE
+        }
+        length_5.setOnClickListener {
+            lining.visibility = GONE
+            thickness.visibility = GONE
+        }
     }
 
     fun initViews() {
