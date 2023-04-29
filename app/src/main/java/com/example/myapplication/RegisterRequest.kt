29ba4cb0baa9708ee7e401874ec54beb2b140bdc
EunseoHan <mam07063@naver.com>
2023-04-29 20:51:29 +0900

    옷 등록(AddCloset)

--- app/src/main/java/com/example/myapplication/RegisterRequest.kt
+++ app/src/main/java/com/example/myapplication/RegisterRequest.kt
@@ -72,8 +72,6 @@ class RegisterRequest(
     }
     companion object {
         // 서버 url 설정 (php 파일 연동)
-        private const val URL = "http://192.168.35.131/Register.php" // "http:// 퍼블릭 DNS 주소/Register.php"
-
-        //private const val URL = "http://192.168.35.43/Register.php"
+        private const val URL = "http://192.168.200.167:8080/Register.php" // "http:// 퍼블릭 DNS 주소/Register.php"
     }
 }
