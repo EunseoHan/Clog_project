b1d7a01e47430b9867d51f8eca573f097808036e
user <cmalice@naver.com>
2023-06-05 00:04:45 +0900

    0605_탈퇴

---
+++ app/src/main/java/com/example/myapplication/Request/ClosetoutRequest.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.Request
+
+import com.android.volley.AuthFailureError
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+
+class ClosetoutRequest(userID: String, listener: Response.Listener<String>) :
+    StringRequest(Method.POST, URL, listener, null) {
+    private val map: MutableMap<String, String>
+
+    init {
+        map = HashMap()
+        map.put("userID",userID)
+    }
+
+    @Throws(AuthFailureError::class)
+    override fun getParams(): Map<String, String>? {
+        return map
+    }
+
+    companion object {
+        // 서버 URL 설정 ( PHP 파일 연동 )
+        private const val URL = "http://121.129.163.76/Closetout.php"
+    }
+}
